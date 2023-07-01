80dabc94b58815a6f4b78588460d3e209dc67774
darkobas2 <marko@datafund.io>
2023-07-01 19:59:54 +0200

    update

--- alerta_falco.py
+++ alerta_falco.py
@@ -12,7 +12,6 @@ from alerta.utils.api import process_alert
 
 JSON = Dict[str, Any]
 
-
 class FalcoWebhook(WebhookBase):
     """
     Falco webhook receiver
@@ -22,30 +21,44 @@ class FalcoWebhook(WebhookBase):
 
       # get values from request params
       environment = query_string.get('environment', current_app.config['DEFAULT_ENVIRONMENT'])
-      service = query_string.get('service', 'Falco').split(',')
       group = query_string.get('group', 'Security')
       customer = query_string.get('customer', None)
       origin = query_string.get('origin', 'Cloud')
-      timeout = query_string.get('timeout', 86400)
-  
-      # get metric labels (evalMatches tags)
-      tags = payload['tags'] or {}
-      severity = payload['priority'].lower()
-  
+
+      # validate timeout value
+      try:
+        timeout = int(query_string.get('timeout', 86400))
+      except ValueError:
+        raise ApiError("Invalid timeout value")
+
+      # ensure required payload fields are present
+      required_fields = ['alert', 'id', 'status']
+      for field in required_fields:
+        if field not in payload:
+          raise ApiError(f"Missing required field in payload: {field}")
+
+      # Extract the hostname
+      hostname = payload['alert']['rawData'].get('hostname', '')
+
+      # get service from request params and add hostname
+      service = payload['alert'].get('service', ['Falco'])
+      if hostname:
+        service.append(hostname)
+
       return Alert(
-          resource=payload['source'],
-          event=payload['rule'],
+          resource=payload['alert'].get('resource', ''),
+          event=payload['alert'].get('event', ''),
           environment=environment,
-          severity=severity,
-          service=service,
+          severity=payload['alert'].get('severity', 'indeterminate').lower(),
+          service=service,  # updated service field
           group=group,
-          value=payload['output_fields'],
-          text=payload['rule'],
-          tags=list(),
+          value=payload['alert'].get('value', ''),
+          text=payload['alert'].get('text', ''),
+          tags=payload['alert'].get('tags', []),  # get tags from the payload
           customer=customer,
           origin=origin,
           event_type='falcoAlert',
           timeout=timeout,
+          repeat=payload['alert'].get('repeat', False),
           raw_data=payload
       )
-
